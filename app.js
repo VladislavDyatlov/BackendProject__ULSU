e388ef5ad314b61b7c0aa8534db4e7229042ebb3
Владислав Дятлов <vladislavdatlov@MacBook-Air-Vladislav.local>
2024-05-17 17:37:44 +0400

    Fix bad rep

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const serverless = require('serverless-http');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
@@ -21,4 +22,6 @@ app.use('/api/curs', require('./routes/curs'));
 app.use('/api/chat', require('./routes/char'));
 app.use('/api/messanger', require('./routes/messanger'));
 
+const handler = serverless(app);
+module.exports = handler;
 module.exports = app;
